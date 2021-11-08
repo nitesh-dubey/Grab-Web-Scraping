4c5caed12331851467910d39fd77f53aad9bbe03
Nitesh Dubey <niteshdubey100@gmail.com>
2021-11-08 18:03:56 +0530

    Solution Updated...

--- README.md
+++ README.md
@@ -4,9 +4,7 @@ This Repository contains the Python script for scraping the Latitude, Longitude,
 ## Contents
 <ul>
   <li><strong>scrape_latlng.py</strong> - <i>Script for Scraping latitude, longitude, name and id of all restaurants at a place in Manila</i></li>
-  <li><strong>output_latlng.json</strong> - <i>Contains the scraped data in JSON format (538 restaurants)</i></li>
-  <li><strong>restaurant_urls.txt</strong> - <i>Contains the urls of all scraped restaurants</i></li>
-</ul>
+  <li><strong>output_latlng.json</strong> - <i>Contains the scraped data in JSON format (538 restaurants)</ul>
 
 ## How to Run
 ```bash
@@ -16,13 +14,21 @@ This Repository contains the Python script for scraping the Latitude, Longitude,
 ## Solution
 The GrabFood Website is made using NextJS with SSR. 
 
-From the [Restaurant List Page](https://food.grab.com/ph/en/), we can get the Latitude and Longitude of only first few restaurants, because the *NEXT_DATA* which is used in hydration of the NextJS app, contains the latitude and longitude of only first few restaurants.
-We don't get the latitudes and longitudes of the subsequent restaurants obtained by clicking *Load More* button.
+On visiting [Restaurant List Page](https://food.grab.com/ph/en/), we can get the Latitude and Longitude of only first 8 restaurants from the *NEXT_DATA*, which is used in hydration of this page.
+We don't get the latitudes and longitudes of the subsequent restaurants obtained by clicking *Load More* button directly.
 
-So to get the Latitude of Longitude of All restaurants on this page, I have coded the following approach using Selenium.
+So to get the Latitude of Longitude of all other restaurants on this page, we need to monitor the requests made to *https://portal.grab.com/foodweb/v2/search* on clicking Load More Button.  The responses of these requests will contain the latlng of all other restaurants.
+
+To Scrape the *latitude*, *longitude*, *name* and *id* of all restaurants,  I've used the following method.
 
 <ul>
-  <li>From the <a href="https://food.grab.com/ph/en/">Restaurant List Page</a> scrape the URLs of all the restaurants, after clicking the <i>Load More</i> button several times to the end of page.</li>
-  
-  <li>Visit each URL, and scrape the Latitude, Longitude, Restaurant Name and ID from the page props.</li>
+
+  <li>On visiting the <a href="https://food.grab.com/ph/en/">Restaurant List Page</a> , scrape the latitudes and  longitudes of first 8 restaurants from <i>NEXT_DATA</i></li>
+
+  <li>Then, after clicking the <i>Load More</i> button several times, till we load to the end of page, capture all the responses from <i>https://portal.grab.com/foodweb/v2/search</i> using selenium-wire.</li>
+
+  <li>Each Response will have the data of 8 restaurants. Scrape the latitudes and longitudes of all restaurants along with their name and Ids from all such responses. </li>
+
+<li>Output the latitudes and longitudes of all restaurants in a<i>JSON</i> file</li>
+
 </ul>
--- scrape_latlng.py
+++ scrape_latlng.py
@@ -1,4 +1,8 @@
-from selenium import webdriver
+# Selenium-wire is used for webdriver instead of Selenium, because we want to monitor
+# the network calls and capture the HTTP requests and response made by the website
+
+from seleniumwire import webdriver
+from seleniumwire.utils import decode
 from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup as bs
 import time
@@ -11,11 +15,10 @@ driver = webdriver.Firefox(options=firefox_options)
 location = "manila"
 
 # Visiting the base_url, and searching for a place in Manila, and getting the list of
-# all restaurants which can deliver food here 
+# all restaurants which can deliver food here
 driver.get('https://food.grab.com/ph/en/')
 search_input = driver.find_element_by_xpath("//*[@class='ant-input']")
 search_button = driver.find_element_by_xpath("//*[@class='ant-btn submitBtn___2roqB ant-btn-primary']")
-
 
 time.sleep(10)
 search_input.send_keys(location)
@@ -26,86 +29,83 @@ search_input.send_keys(Keys.ENTER)
 time.sleep(10)
 search_button.click()
 
+time.sleep(10)
 
-# Since we want all the restaurants on this page so we need to click
-# Load More till the end of page
+restaurant_list = []
+# After the Restaurant List page is loaded, we can get the latitude and longitude
+# of first 8 restaurants from the __NEXT_DATA__
+
+soup = bs(driver.page_source, "html.parser")
+next_data = json.loads(soup.find(id="__NEXT_DATA__").text)
+initial_restaurant_dict = next_data['props']['initialReduxState']['pageRestaurantsV2']['entities']['restaurantList']
+for restaurant_id, restaurant in initial_restaurant_dict.items():
+    _data = {}
+    _data['id'] = restaurant['id']
+    _data['name'] = restaurant['name']
+    _data['latitude'] = restaurant['latitude']
+    _data['longitude'] = restaurant['longitude']
+
+    restaurant_list.append(_data)
+
+
+# Since we want latlng all the restaurants on this page so we need to click
+# Load More till we load to the end of this page
+
+# Since the latlng of only first 8 restaurants are present in __NEXT_DATA__ but we want
+# The latlng of all the restaurants on this page, so we need to monitor the requests made
+# on clicking Load More button to 'https://portal.grab.com/foodweb/v2/search'
+# The response of this request will contain the latlng of next 8 restaurants
+
 # We also need to sleep between clicks to prevent being detected as bots
 
-time.sleep(10)
 load_more_button = driver.find_element_by_xpath("//*[@class='ant-btn ant-btn-block']")
+time.sleep(10)
 
-
+click_num = 1
 while True:
     try:
-        time.sleep(10)
         load_more_button.click()
+
+        print(f'Click Number : {click_num}')
+
+        if(click_num % 40 == 0):
+            time.sleep(600)
+        elif(click_num % 15 == 0):
+            time.sleep(300)
+        else:
+            time.sleep(10)
+
+        click_num += 1
     except:
         break
 
 
 time.sleep(10)
 
-# After the whole page is loaded now, scrape all the urls of restaurants on this page
 
-# This list will store urls of all restaurants on this page
-restaurant_urls = []
-soup = bs(driver.page_source, "html.parser")
+# Finding all the responses from 'https://portal.grab.com/foodweb/v2/search'
+# and getting the latitude, longitude, id and names of all restaurants on this page.
 
-base_url = 'https://food.grab.com'
-for parent in soup.select("div.ant-col-24.RestaurantListCol___1FZ8V.ant-col-md-12.ant-col-lg-6"):
-    a_tag = parent.find("a")
-    href = a_tag.attrs['href']
-    url = base_url + href
-    restaurant_urls.append(url)
+for request in driver.requests:
+    if request.response and request.url == 'https://portal.grab.com/foodweb/v2/search':
+        res = request.response
+        body = decode(res.body, res.headers.get('Content-Encoding', 'identity'))
+        body_json = json.loads(body)
 
+        searchMerchants = body_json['searchResult']['searchMerchants']
+        for restaurant in searchMerchants:
+            _data = {}
+            _data['id'] = restaurant['id']
+            _data['name'] = restaurant['address']['name']
+            _data['latitude'] = restaurant['latlng']['latitude']
+            _data['longitude'] = restaurant['latlng']['longitude']
 
-# Saving Restaurant urls
-with open('restaurant_urls.txt', 'w') as f:
-    for item in restaurant_urls:
-        f.write('%s\n' % item)
+            restaurant_list.append(_data)
 
-
-# Total Number of Restaurants
-print(f'Total Restaurants : {len(restaurant_urls)}\n')
-
-# Output file containing latitude, longitude, name and id of all restaurants on this page
-output_json = {"_total_restaurants": len(restaurant_urls), "restaurant_list": []}
-
-restaurant_number = 1
-for url in restaurant_urls:
-
-    print(f'Scraping Restaurant Number : {restaurant_number} -----------------------------------------')
-
-    driver.get(url)
-    soup = bs(driver.page_source, 'html.parser')
-    next_data = json.loads(soup.find(id="__NEXT_DATA__").text)
-
-    restaurant_id = url.split('/')[-1]
-    restaurant = next_data["props"]["initialReduxState"]["pageRestaurantDetail"]["entities"][restaurant_id]
-    restaurant_name = restaurant["name"]
-    restaurant_latitude = restaurant["latlng"]["latitude"]
-    restaurant_longitude = restaurant["latlng"]["longitude"]
-
-    data = {}
-    data["id"] = restaurant_id
-    data["name"] = restaurant_name
-    data["latitude"] = restaurant_latitude
-    data["longitude"] = restaurant_longitude
-
-    output_json["restaurant_list"].append(data)
-
-    # Waiting for sometime, to avoid flooding the website with requests
-    if(restaurant_number % 50 == 0):
-        time.sleep(15)
-    else:
-        time.sleep(5)
-
-    restaurant_number += 1
+output_latlng = {"_total_restaurants": len(restaurant_list), "restaurant_list": restaurant_list}
 
 
-# Saving the output file
 with open('output_latlng.json', 'w') as f:
-    json.dump(output_json, f, ensure_ascii=False, indent=4)
-
+    json.dump(output_latlng, f, ensure_ascii=False, indent=4)
 
 print('\nDone...')
